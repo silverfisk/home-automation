15610bf6622f87c9da953e45e57fbbafb7c52528
Nordmark <tom@linux-konsult.com>
2014-03-03 21:16:09 +0100

    corrected typo

--- pumpstatus.sh
+++ pumpstatus.sh
@@ -2,4 +2,4 @@
 
 STATUS=$(/usr/bin/tdtool --list |egrep '^4' |awk '{print $7}')
 
-echo "Heat pump statis is: $STATUS. Status last checked $(date)"
+echo "Heat pump status is: $STATUS. Status last checked $(date)"
