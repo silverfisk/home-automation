d63b50bf10268d5d45ed40ebb092078066691b84
Nordmark <tom@linux-konsult.com>
2014-03-03 21:04:38 +0100

    The pump should be turned of during night time

--- tempAutomation.rb
+++ tempAutomation.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/env ruby
 # Tom Eklöf, tom@linux-konsult.com
-# Control a heat circulation pump with a TellStick and usb termometer (tdtool and temperv14 have to be installed)
+# Control a heat pump with a TellStick and usb termometer (tdtool and temperv14 have to be installed)
 
 require 'date'
 
 ####	Settings	####
 interval = 1800					# Number of seconds we loop for temp logging and adjustments
 tdunit = 4					# tdtool --list gives you the correct unit number
-temptarget = 22.5				# Our target temperature
+temptarget = 22.0				# Our target temperature
+pump_skip_hours = [ 0, 1, 2, 3, 4, 5, 6, 7, 22, 23 ]	# Don't run the pump on these hours
 ####	/Settings	####
 
 ### 	Functions	####
@@ -28,8 +29,24 @@ end
 loop do
  time = DateTime.now
 
- # Get Temp and log it
- livingroom = get_temp
+ # dont run the pump at nights but log the temp
+ hour = DateTime.now.hour
+  pump_skip_hours.each do |sleep|
+  hour = DateTime.now.hour
+   if hour == sleep
+    livingroom = get_temp			# Get Temp and log it
+    open('/root/templog.csv', 'a+') do |t|
+     t.puts "#{time}	#{livingroom}"
+    end
+    sleep interval
+    hour = DateTime.now.hour
+    redo
+   end
+  end
+
+
+
+ livingroom = get_temp				# Get Temp and log it
  # Log the temperature
  open('/root/templog.csv', 'a+') do |t|
   t.puts "#{time}	#{livingroom}"
