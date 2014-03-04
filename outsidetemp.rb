81c837da5ec7b0d6874537504fc359513e29bc4f
Nordmark <tom@linux-konsult.com>
2014-03-04 22:16:16 +0100

    Added long/lat variables

---
+++ outsidetemp.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'date'
+
+key = ''					# the worldweatheronline.com free key
+interval = 900					# Loop interval
+long = '58.970'
+lat = '17.405'
+
+# Loop forever
+loop do
+  # Get outside temp in C
+  temp = %x{/usr/bin/curl -s 'http://api.worldweatheronline.com/free/v1/weather.ashx?q=#{long}%2C#{lat}&format=json&num_of_days=1&key=#{key}'|awk -F: '{print $10}'|awk -F, '{print $1}'| awk -F\\" '{print $2}'}.to_i
+  # Log the temp to logfile
+  open('/root/outsidetemp.csv', 'a+') do |f|
+   f.puts "#{DateTime.now}	#{temp}"
+  end
+  sleep interval
+end
