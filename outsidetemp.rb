1021ce5c408fd7105660ed65483020d68f01ae8f
Nordmark <tom@linux-konsult.com>
2014-03-07 20:56:50 +0100

    Ooops, mixed up long/lat

--- outsidetemp.rb
+++ outsidetemp.rb
@@ -3,13 +3,13 @@ require 'date'
 
 key = ''					# the worldweatheronline.com free key
 interval = 900					# Loop interval
-long = '58.970'
-lat = '17.405'
+lat = '58.970'
+long = '17.405'
 
 # Loop forever
 loop do
   # Get outside temp in C
-  temp = %x{/usr/bin/curl -s 'http://api.worldweatheronline.com/free/v1/weather.ashx?q=#{long}%2C#{lat}&format=json&num_of_days=1&key=#{key}'|awk -F: '{print $10}'|awk -F, '{print $1}'| awk -F\\" '{print $2}'}.to_i
+  temp = %x{/usr/bin/curl -s 'http://api.worldweatheronline.com/free/v1/weather.ashx?q=#{lat}%2C#{long}&format=json&num_of_days=1&key=#{key}'|awk -F: '{print $10}'|awk -F, '{print $1}'| awk -F\\" '{print $2}'}.to_i
   # Log the temp to logfile
   open('/root/outsidetemp.csv', 'a+') do |f|
    f.puts "#{DateTime.now}	#{temp}"
