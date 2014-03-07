f8f5493d85b88a0f39f1aedc0b1ab52bf830fa89
Nordmark <tom@linux-konsult.com>
2014-03-07 21:36:34 +0100

    Generate a html page with pump status and diagram graphs

--- pumpstatus.sh
+++ pumpstatus.sh
@@ -2,4 +2,14 @@
 
 STATUS=$(/usr/bin/tdtool --list |egrep '^4' |awk '{print $7}')
 
-echo "Heat pump status is: $STATUS. Status last checked $(date)"
+
+echo "<html>
+<body>
+<center>
+ <h1>Temperatures and controls</h1>
+ Heat circulation pump status is: <b> $STATUS </b>. Status was last checked $(date)
+ <img src=\"./livingroom.png\" />
+ <img src=\"./outside.png\" />
+</center>
+</body>
+</html>"
